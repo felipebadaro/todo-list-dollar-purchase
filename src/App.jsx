ff3ed342d097dd81b3ec55ec99926c92b139ada7
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-03-12 16:24:04 -0300

    mais ajustes visuais e início do menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 import "./App.css";
 import Form from "./Form";
+import Menu from "./Menu";
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Form />
     </div>
   );
---
+++ src/Menu.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <div className="MenuWrapper">
+      <ul>
+        <li>Cadastro</li>
+        <li>Compras</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
