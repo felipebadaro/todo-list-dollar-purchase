919bf1557b6723fc19bf75721c189a9f7d1b69bd
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-03-14 19:39:59 -0300

    faz ajustes visuais na lista de compras e nas compras

--- src/components/PurchasesList.jsx
+++
@@ -1,9 +0,0 @@
-function PurchasesList() {
-  return (
-    <div className="purchasesWrapper">
-      <p>Dentro do PurchasesList</p>
-    </div>
-  );
-}
-
-export default PurchasesList;
--- src/routes/Purchases.jsx
+++ src/routes/Purchases.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import PurchaseList from "../components/PurchaseList";
 
 const Purchases = () => {
-  return <div>Purchases</div>;
+  return <PurchaseList />;
 };
 
 export default Purchases;
